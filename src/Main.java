82565376743cdb0565d7746e9aec72272baadda5
Sohan Singhamahapatra <sohansinghamahapatra02103@gmail.com>
2025-10-19 15:55:56 +0530

    class 1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // variables and datatypes
+
+        // datatupe <name> = <value>
+        int age =23;
+        // data types : premitive and non premitive data types
+        // premitive : int , char , boolean , string , float , double , bit etc.
+        // non premitive : arrays , stack , queues
+
+        char c='c';
+        String str="sanglap";
+        boolean b= false;
+        float f=1.2f;
+        double d=12.3;
+
+        // operators : mathematical and logical operator
+
+//        math : + - * / %
+
+        int num1 =11;
+        int num2=5;
+
+        System.out.println("the sum of num1 and num2 is "+(num1 + num2));
+        System.out.println("the substraction of num1 and num2 is "+(num1 - num2));
+        System.out.println("the multiply of num1 and num2 is "+(num1 * num2));
+        System.out.println("the devide of num1 and num2 is "+(num1 / num2));
+        System.out.println("the devide of num1 and num2 is "+(num1 % num2));
+
+        // logical operators
+
+        // or and not
+
+        // conditional statements
+
+
+
+
+    }
+
+}
