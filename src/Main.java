d2cc0afb7c85432309672fbb5624989ffc9b4f72
Sohan Singhamahapatra <sohansinghamahapatra02103@gmail.com>
2025-10-26 17:40:11 +0530

    class2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
 
@@ -24,20 +25,98 @@ public class Main {
         int num1 =11;
         int num2=5;
 
-        System.out.println("the sum of num1 and num2 is "+(num1 + num2));
-        System.out.println("the substraction of num1 and num2 is "+(num1 - num2));
-        System.out.println("the multiply of num1 and num2 is "+(num1 * num2));
-        System.out.println("the devide of num1 and num2 is "+(num1 / num2));
-        System.out.println("the devide of num1 and num2 is "+(num1 % num2));
+//        System.out.println("the sum of num1 and num2 is "+(num1 + num2));
+//        System.out.println("the substraction of num1 and num2 is "+(num1 - num2));
+//        System.out.println("the multiply of num1 and num2 is "+(num1 * num2));
+//        System.out.println("the devide of num1 and num2 is "+(num1 / num2));
+//        System.out.println("the devide of num1 and num2 is "+(num1 % num2));
 
         // logical operators
 
-        // or and not
+        // or and not : && ,|| , ~
 
         // conditional statements
 
+        // take an input from user of his age ,
+
+        Scanner sc=new Scanner(System.in);
 
 
+//        System.out.println("Enter your age : ");
+//        int age_new = sc.nextInt();
+
+//        System.out.println("Enter your name : ");
+//        String name=sc.next();
+
+
+//        System.out.println("The user's age is : "+age_new);
+//        System.out.println("The user's name is : "+name);
+
+        // conditional statement
+
+//        if( age_new > 18 ) {
+//            System.out.println("you are eligible");
+//        }
+//        else {
+//            System.out.println("you are not eligible");
+//        }
+
+       // nested conditional statements
+//        if(age_new >18){
+//
+//            if(age_new < 60){
+//                System.out.println("eligible");
+//            }
+//            else{
+//                System.out.println("not eligible");
+//            }
+//
+//        }
+//        else{
+//            System.out.println("not eligible");
+//        }
+
+
+        // logical operator
+
+//        if ( (age_new > 18) && (age_new <60) ){
+//            System.out.println("eligible");
+//        }
+//        else{
+//            System.out.println("not eligible");
+//        }
+
+        // loops : for , while , do while
+
+
+//        int i=1;
+//        ++i -> first increment it then use it ->pre increment
+//        i++ -> first use it then increment it
+
+//        System.out.println(i++);
+//
+//        System.out.println(i);
+
+
+
+//        for(int i=1 ; i<=10 ; i++){
+//            System.out.println(i);
+//        }
+//        System.out.println(i); --> scope
+
+//        int i=1;
+//
+//        while (i<=10){
+//            System.out.println(i);
+//            i++;
+//        }
+
+
+        // 1. take user's accaunt balance as input , if the balance is > 10,000 you are eligble for the card , else you are not eligible
+
+        // 2. take a fruit name as input from user , if the fruit name is apple or banana then print eat , else print dont eat
+
+        // 3. take a integer input from user and print it's table
 
     }
 
